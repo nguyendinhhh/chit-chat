a9dc5116dc521cfcefa63ccdc02914a355f398de
Nguyen Dinh <nguyenhpdinh@gmail.com>
2022-09-20 11:09:52 -0700

    revised login

--- frontend/src/components/authentication/Login.js
+++ frontend/src/components/authentication/Login.js
@@ -12,23 +12,22 @@ import {
 import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { ChatState } from "../../Context/ChatProvider";
-	
-const Login = () => {
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
-	const [show, setShow] = useState(false);
-	const [loading, setLoading] = useState(false);
 
+const Login = () => {
+	const [show, setShow] = useState(false);
 	const handleClick = () => setShow(!show);
 	const toast = useToast();
-	const history = useHistory();
-	const { setUser, setInitialLoading } = ChatState();
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+	const [loading, setLoading] = useState(false);
 
-	const submitHandler = async (e) => {
+	const history = useHistory();
+
+	const submitHandler = async () => {
 		setLoading(true);
 		if (!email || !password) {
 			toast({
-				title: "Please fill out all fields",
+				title: "Please Fill all the Feilds",
 				status: "warning",
 				duration: 5000,
 				isClosable: true,
@@ -37,18 +36,19 @@ const Login = () => {
 			setLoading(false);
 			return;
 		}
-
 		try {
 			const config = {
 				headers: {
 					"Content-type": "application/json",
 				},
 			};
+
 			const { data } = await axios.post(
 				"/api/user/login",
 				{ email, password },
 				config
 			);
+
 			toast({
 				title: "Login Successful",
 				status: "success",
@@ -56,19 +56,13 @@ const Login = () => {
 				isClosable: true,
 				position: "bottom",
 			});
-			setInitialLoading(true);
-			// setUser(data);
-			localStorage.setItem("userInfo", JSON.stringify(data)); // for context api. Check ChatProvider.js
+			localStorage.setItem("userInfo", JSON.stringify(data));
 			setLoading(false);
 			history.push("/chats");
-			setTimeout(() => {
-				setInitialLoading(false);
-			}, 2000);
 		} catch (error) {
-			console.log(error);
 			toast({
 				title: "Error Occured!",
-				description: error,
+				description: error.response.data.message,
 				status: "error",
 				duration: 5000,
 				isClosable: true,
@@ -79,25 +73,26 @@ const Login = () => {
 	};
 
 	return (
-		<VStack spacing="5px">
+		<VStack spacing="10px">
 			<FormControl id="email" isRequired>
-				<FormLabel>Email</FormLabel>
+				<FormLabel>Email Address</FormLabel>
 				<Input
-					placeholder="Enter Your Email"
-					value={email} // after adding this line, if user clicks get guest creds button, the field is automatically filled, thanks to useState
+					value={email}
+					type="email"
+					placeholder="Enter Your Email Address"
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 			</FormControl>
 			<FormControl id="password" isRequired>
 				<FormLabel>Password</FormLabel>
-				<InputGroup>
+				<InputGroup size="md">
 					<Input
-						type={show ? "text" : "password"}
-						placeholder="Password"
-						value={password} // after adding this line, if user clicks get guest creds button, the field is automatically filled, thanks to useState
+						value={password}
 						onChange={(e) => setPassword(e.target.value)}
+						type={show ? "text" : "password"}
+						placeholder="Enter password"
 					/>
-					<InputRightElement width="4.5em">
+					<InputRightElement width="4.5rem">
 						<Button h="1.75rem" size="sm" onClick={handleClick}>
 							{show ? "Hide" : "Show"}
 						</Button>
@@ -105,7 +100,7 @@ const Login = () => {
 				</InputGroup>
 			</FormControl>
 			<Button
-				colorScheme={"pink"}
+				colorScheme="pink"
 				width="100%"
 				style={{ marginTop: 15 }}
 				onClick={submitHandler}
@@ -113,20 +108,123 @@ const Login = () => {
 			>
 				Login
 			</Button>
-			{/* <Button
-				variant="solid"
-				colorScheme={"gray"}
-				width="100%"
-				style={{ marginTop: 15 }}
-				onClick={() => {
-					setEmail("guest@example.com");
-					setPassword("mypassword");
-				}}
-			>
-				Get Guest User Credentials
-			</Button> */}
 		</VStack>
 	);
 };
+// const Login = () => {
+// 	const [email, setEmail] = useState();
+// 	const [password, setPassword] = useState();
+// 	const [show, setShow] = useState(false);
+// 	const [loading, setLoading] = useState(false);
+
+// 	const handleClick = () => setShow(!show);
+// 	const toast = useToast();
+// 	const history = useHistory();
+// 	const { setUser, setInitialLoading } = ChatState();
+
+// 	const submitHandler = async (e) => {
+// 		setLoading(true);
+// 		if (!email || !password) {
+// 			toast({
+// 				title: "Please fill out all fields",
+// 				status: "warning",
+// 				duration: 5000,
+// 				isClosable: true,
+// 				position: "bottom",
+// 			});
+// 			setLoading(false);
+// 			return;
+// 		}
+
+// 		try {
+// 			const config = {
+// 				headers: {
+// 					"Content-type": "application/json",
+// 				},
+// 			};
+// 			const { data } = await axios.post(
+// 				"/api/user/login",
+// 				{ email, password },
+// 				config
+// 			);
+// 			toast({
+// 				title: "Login Successful",
+// 				status: "success",
+// 				duration: 5000,
+// 				isClosable: true,
+// 				position: "bottom",
+// 			});
+// 			setInitialLoading(true);
+// 			setUser(data);
+// 			localStorage.setItem("userInfo", JSON.stringify(data)); // for context api. Check ChatProvider.js
+// 			setLoading(false);
+// 			history.push("/chats");
+// 			setTimeout(() => {
+// 				setInitialLoading(false);
+// 			}, 2000);
+// 		} catch (error) {
+// 			console.log(error);
+// 			toast({
+// 				title: "Error Occured!",
+// 				description: error,
+// 				status: "error",
+// 				duration: 5000,
+// 				isClosable: true,
+// 				position: "bottom",
+// 			});
+// 			setLoading(false);
+// 		}
+// 	};
+
+// 	return (
+// 		<VStack spacing="5px">
+// 			<FormControl id="email" isRequired>
+// 				<FormLabel>Email</FormLabel>
+// 				<Input
+// 					placeholder="Enter Your Email"
+// 					value={email} // after adding this line, if user clicks get guest creds button, the field is automatically filled, thanks to useState
+// 					onChange={(e) => setEmail(e.target.value)}
+// 				/>
+// 			</FormControl>
+// 			<FormControl id="password" isRequired>
+// 				<FormLabel>Password</FormLabel>
+// 				<InputGroup>
+// 					<Input
+// 						type={show ? "text" : "password"}
+// 						placeholder="Password"
+// 						value={password} // after adding this line, if user clicks get guest creds button, the field is automatically filled, thanks to useState
+// 						onChange={(e) => setPassword(e.target.value)}
+// 					/>
+// 					<InputRightElement width="4.5em">
+// 						<Button h="1.75rem" size="sm" onClick={handleClick}>
+// 							{show ? "Hide" : "Show"}
+// 						</Button>
+// 					</InputRightElement>
+// 				</InputGroup>
+// 			</FormControl>
+// 			<Button
+// 				colorScheme={"pink"}
+// 				width="100%"
+// 				style={{ marginTop: 15 }}
+// 				onClick={submitHandler}
+// 				isLoading={loading}
+// 			>
+// 				Login
+// 			</Button>
+// 			{/* <Button
+// 				variant="solid"
+// 				colorScheme={"gray"}
+// 				width="100%"
+// 				style={{ marginTop: 15 }}
+// 				onClick={() => {
+// 					setEmail("guest@example.com");
+// 					setPassword("mypassword");
+// 				}}
+// 			>
+// 				Get Guest User Credentials
+// 			</Button> */}
+// 		</VStack>
+// 	);
+// };
 
 export default Login;
